b58b6c2ba12327b97b3048fb5b3a60ea6154ff83
Caged <justin@labratrevenge.com>
2011-06-12 15:11:26 -0700

    add rules, cleanup design

--- cof/app.coffee
+++ cof/app.coffee
@@ -1,16 +1,17 @@
 $ ->
   days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  data = [[10, 5, 10, 5, 8, 15, 18], [20, 10, 20, 40, 23, 14, 12]]
+  data = [[10, 5, 10, 5, 8, 15, 18], [20, 10, 20, 40, 23, 20, 12]]
   
-  [pt, pl, pr, pb] = [20, 20, 20, 20]  # padding
+  [pt, pl, pr, pb] = [20, 35, 20, 25]  # padding
   w = $('#chart').width()  - (pl + pr) # width
   h = $('#chart').height() - (pt + pb) # height
+  tl = 3 # tick length
   
   max = d3.max(d3.merge(data))
 
   # Scales
   x  = d3.scale.linear().domain([0, data[0].length - 1]).range [0, w]
-  y  = d3.scale.linear().domain([0, max]).range [h, 0]
+  y  = d3.scale.linear().domain([0, max + 5]).range [h, 0]
   xl = d3.scale.ordinal().domain(d3.range(days.length)).rangeRoundBands([0, w], 0)
 
   # Base vis layer
@@ -39,7 +40,28 @@ $ ->
     .text((d) -> d)
     .attr('y', h)
     .attr("dy", "1.5em")
-    .attr('dx', "15px")
+    .attr('dx', (d) -> (xl.rangeBand() / 2.0) - (this.textLength.baseVal.value / 2.0))
+  
+  # Add tick groups
+  ticks = vis.selectAll('.tick')
+    .data(y.ticks(10))
+  .enter().append('svg:g')
+    .attr('transform', (d) -> "translate(-#{tl * 1.5}, #{y(d)})")
+    .attr('class', 'tick')
+    
+  # Add y axis tick marks
+  ticks.append('svg:line')
+    .attr('y1', 0)
+    .attr('y2', 0)
+    .attr('x1', 0)
+    .attr('x2', w + tl)
+      
+  # Add y axis tick labels
+  ticks.append('svg:text')
+    .text((d) -> d)
+    .attr('text-anchor', 'end')
+    .attr('dy', 2)
+    .attr('x', -4)
     
   # Layer group for paths and their points
   lines = vis.selectAll('line.group')
@@ -64,4 +86,3 @@ $ ->
     .attr("cx", (d, i) -> x(i))
     .attr("cy", (d) -> y(d))
     .attr("r", 4)
-  
--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 (function() {
   $(function() {
-    var bars, data, days, h, lines, max, pb, pl, pr, pt, vis, w, x, xl, y, _ref;
+    var bars, data, days, h, lines, max, pb, pl, pr, pt, ticks, tl, vis, w, x, xl, y, _ref;
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    data = [[10, 5, 10, 5, 8, 15, 18], [20, 10, 20, 40, 23, 14, 12]];
-    _ref = [20, 20, 20, 20], pt = _ref[0], pl = _ref[1], pr = _ref[2], pb = _ref[3];
+    data = [[10, 5, 10, 5, 8, 15, 18], [20, 10, 20, 40, 23, 20, 12]];
+    _ref = [20, 35, 20, 25], pt = _ref[0], pl = _ref[1], pr = _ref[2], pb = _ref[3];
     w = $('#chart').width() - (pl + pr);
     h = $('#chart').height() - (pt + pb);
+    tl = 3;
     max = d3.max(d3.merge(data));
     x = d3.scale.linear().domain([0, data[0].length - 1]).range([0, w]);
-    y = d3.scale.linear().domain([0, max]).range([h, 0]);
+    y = d3.scale.linear().domain([0, max + 5]).range([h, 0]);
     xl = d3.scale.ordinal().domain(d3.range(days.length)).rangeRoundBands([0, w], 0);
     vis = d3.select('#chart').append('svg:svg').attr('width', w + (pl + pr)).attr('height', h + pt + pb).attr('class', 'viz').append('svg:g').attr('transform', "translate(" + pl + "," + pt + ")");
     bars = vis.selectAll('g.bar').data(days).enter().append('svg:g').attr('transform', function(d, i) {
@@ -23,7 +24,16 @@
     bars.append("svg:rect").attr("width", xl.rangeBand()).attr("height", h);
     bars.append('svg:text').text(function(d) {
       return d;
-    }).attr('y', h).attr("dy", "1.5em").attr('dx', "15px");
+    }).attr('y', h).attr("dy", "1.5em").attr('dx', function(d) {
+      return (xl.rangeBand() / 2.0) - (this.textLength.baseVal.value / 2.0);
+    });
+    ticks = vis.selectAll('.tick').data(y.ticks(10)).enter().append('svg:g').attr('transform', function(d) {
+      return "translate(-" + (tl * 1.5) + ", " + (y(d)) + ")";
+    }).attr('class', 'tick');
+    ticks.append('svg:line').attr('y1', 0).attr('y2', 0).attr('x1', 0).attr('x2', w + tl);
+    ticks.append('svg:text').text(function(d) {
+      return d;
+    }).attr('text-anchor', 'end').attr('dy', 2).attr('x', -4);
     lines = vis.selectAll('line.group').data(data).enter().append('svg:g').attr('class', function(d, i) {
       if (i === 0) {
         return 'cats';
